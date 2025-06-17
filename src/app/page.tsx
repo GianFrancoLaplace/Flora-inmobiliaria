f7e902f7c5d7adc2641fe7a2b9ab9908736dd3fe
Agostina Bidegain <agostinabidegain@gmail.com>
2025-06-17 00:11:51 -0300

    implemented footer and whatsapp redirection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,10 @@ export default function Home() {
       <main className={styles.main}>
         <Image
           className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
+          src="/magnoLogo.png"
+          alt="Magno logo"
+          width={198}
+          height={200}
           priority
         />
         <ol>
@@ -23,76 +23,23 @@ export default function Home() {
         <div className={styles.ctas}>
           <a
             className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://www.figma.com/design/edBzcuBuqGyKY1sBo6IyLI/floraCordeiro?node-id=0-1&p=f&t=T85jrtBxhqXGDgcf-0"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+            Figma
           </a>
           <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://trello.com/b/TTUjxt0R/floracordeiroinmobiliaria"
             target="_blank"
             rel="noopener noreferrer"
             className={styles.secondary}
           >
-            Read our docs
+
+            Trello
           </a>
         </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-        <a href="/PruebaInmobiliaria">
-          Inmobiliaria
-        </a>
-      </footer>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,63 @@
+import styles from './Footer.module.css';
+import Image from "next/image";
+
+export default function Footer(){
+    return (
+        <footer className={styles.footer}>
+            <div className={styles.footerSection}>
+                <h2>Redes sociales</h2>
+                <div className={styles.footerImgContent}>
+                    <Image
+                        src={'/redes/instagram.png'}
+                        alt={'instagram logo (social media)'}
+                        width={30}
+                        height={30}
+                    />
+                    <h5>floracordeiro_inmobiliaria</h5>
+                </div>
+                <div className={styles.footerImgContent}>
+                    <Image
+                        src={'/redes/facebook.png'}
+                        alt={'facebook logo (social media)'}
+                        width={30}
+                        height={30}
+                    />
+                    <h5>Flora Cordeiro Inmobiliaria</h5>
+                </div>
+                <div className={styles.footerImgContent}>
+                    <Image
+                        src={'/redes/whatsapp.png'}
+                        alt={'whatsapp logo (social media)'}
+                        width={30}
+                        height={30}
+                    />
+                    <h5>Flora Cordeiro</h5>
+                </div>
+            </div>
+
+            <div className={styles.footerSection}>
+                <h2>Contacto</h2>
+                <h5 className={styles.footerImgContent}>2494 11-2233</h5>
+                <h5 className={styles.footerImgContent}>Alsina 421, Tandil.</h5>
+                <div className={styles.footerImgContent}>
+                    <Image
+                        src={'/redes/gmail.png'}
+                        alt={'gmail logo (social media)'}
+                        width={30}
+                        height={30}
+                    />
+                    <h5>floracordeiroinmobiliaria@gmail.com</h5>
+                </div>
+            </div>
+
+            <div className={styles.footerSection}>
+                <Image
+                    src={'/footerLogo.png'}
+                    alt={'minimalist inmobiliaria yellow logo'}
+                    width={200}
+                    height={120}
+                />
+            </div>
+        </footer>
+    )
+}
