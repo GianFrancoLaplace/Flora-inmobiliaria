3ef84de1329440b01c8e87b26be029980eb992ab
milagros alvarez <milagrosalvarez2604@gmail.com>
2025-06-20 20:40:17 -0300

    pequeño arreglo de estilos en propiedades

--- src/app/Propiedades/page.tsx
+++ src/app/Propiedades/page.tsx
@@ -3,6 +3,8 @@ import ContactInformation from "@/components/features/ContactInformation/Contact
 import FilterGroup from "../../components/FilterButtons/filtergroup";
 import './properties-styles.css';
 import { useState } from "react";
+import '../ui/fonts';
+import { cactus } from "../ui/fonts";
 
 
 export default function Properties() {
@@ -23,49 +25,54 @@ export default function Properties() {
         setMaxValue(e.target.value);
     }
 
-   return (
-    <div>
-        <main>
-            <ContactInformation />
-        </main>
-        <br />
-        <div className="properties-layout">
-            <div className="container-filter-properties">
-                {/* Input para valor máximo */}
-                <div className="filter-container">
-                    <div className="flex-col">
-                        <label htmlFor="maxValueInput" className="filter-section-title">Valor máximo</label>
-                        <input
-                            id="maxValueInput"
-                            type="number"
-                            className="max-value-input"
-                            placeholder="Escribe el valor máximo"
-                            value={maxValue}
-                            onChange={handleMaxValueChange}
+    return (
+        <div>
+            <main>
+                <ContactInformation />
+            </main>
+            <br />
+            <div className="properties-layout">
+                <div className="container-filter-properties">
+                    {/* Input para valor máximo */}
+                    <div className="filter-container">
+                        <div className="flex-col">
+                            <label
+                                htmlFor="maxValueInput"
+                                className={`filter-section-title ${cactus.className} antialiased`}
+                            >
+                                Valor máximo
+                            </label>
+                            <input
+                                id="maxValueInput"
+                                type="number"
+                                className="max-value-input"
+                                placeholder="Escribe el valor máximo"
+                                value={maxValue}
+                                onChange={handleMaxValueChange}
+                            />
+                        </div>
+                    </div>
+
+                    <div className="filters-column">
+                        <FilterGroup
+                            title="Filtrar por operación"
+                            filters={filtrosTipoTransaccion}
+                            direction="column"
+                        />
+
+                        <FilterGroup
+                            title="Filtrar por inmueble"
+                            filters={filtrosTipoPropiedad}
+                            direction="column"
                         />
                     </div>
                 </div>
 
-                <div className="filters-column">
-                    <FilterGroup
-                        title="Filtrar por operación"
-                        filters={filtrosTipoTransaccion}
-                        direction="column"
-                    />
-
-                    <FilterGroup
-                        title="Filtrar por inmueble"
-                        filters={filtrosTipoPropiedad}
-                        direction="column"
-                    />
+                <div className="properties-list-container">
+                    <p>Futuro listado de propiedades:</p>
+                    <p>Valor máximo seleccionado: {maxValue}</p>
                 </div>
             </div>
-
-            <div className="properties-list-container">
-                <p>Futuro listado de propiedades:</p>
-                <p>Valor máximo seleccionado: {maxValue}</p>
-            </div>
         </div>
-    </div>
-);
+    );
 }
