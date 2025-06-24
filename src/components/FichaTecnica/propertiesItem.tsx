2ff0eca6dabebceac2abbd5753b8616c23a071da
Agostina Bidegain <agostinabidegain@gmail.com>
2025-06-24 15:39:24 -0300

    asigned icons for each ficha property

---
+++ src/components/FichaTecnica/propertiesItem.tsx
@@ -0,0 +1,22 @@
+import styles from "@/components/FichaTecnica/FichaTecnica.module.css";
+import Image from "next/image";
+
+type Props = {
+    imgSrc: string;
+    label: string;
+    value: string;
+};
+
+export default function Item({ imgSrc, label, value }: Props) {
+    return (
+        <div className={styles.itemProperties}>
+            <Image
+                src={imgSrc}
+                alt={'icono acorde a la informacion proporcionada'}
+                width={20}
+                height={20}
+            />
+            <h5>{label}: {value}</h5>
+        </div>
+    )
+}
