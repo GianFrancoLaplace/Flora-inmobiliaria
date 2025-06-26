54933e4ad9a9b927a167402e1c01f9388ff51157
milagros alvarez <milagrosalvarez2604@gmail.com>
2025-06-26 14:40:57 -0300

    arreglo detalle del nav fijo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,7 +48,6 @@ export default function Page() {
                     <FilterGroup
                         title=""
                         filters={filtrosTipoPropiedad}
-                        direction="row"
                     />
                 </div>
 
