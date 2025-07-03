15525c0d2c61c9044b682ed5e63e8cace27c6223
Agostina Bidegain <agostinabidegain@gmail.com>
2025-07-02 23:25:37 -0300

    add send message and see all properties buttons at home section

--- src/components/BigCards/BigCardsGrid.tsx
+++ src/components/BigCards/BigCardsGrid.tsx
@@ -7,15 +7,6 @@ import React from "react";
 export default function BigCardsGrid() {
     return(
         <main className={`${styles.grid} ${cactus.className}`}>
-            {/*
-            <div
-                className={styles.messageButtonProperties}>
-                <Link href={"https://wa.me/2494025527"} className={styles.linkProperties}>
-                    <button className={`${styles.messageBtn} ${cactus.className}`}>Enviar un mensaje
-                    </button>
-                </Link>
-            </div>
-            */}
 
             <BigCard
                 imageSrc={"/backgrounds/fichaBackground.jpg"}
@@ -87,15 +78,6 @@ export default function BigCardsGrid() {
                 dorms={3}
                 bathrooms={2}
             />
-
-            {/*
-            <div className={`${styles.mainCardsGridProperties} ${isHome ? styles.viewButton : styles.notViewButton}`}>
-                <Link href={"/Propiedades"} className={styles.linkProperties}>
-                    <button className={`${styles.allPropertiesBtn} ${cactus.className}`}>Ver todas las propiedades
-                    </button>
-                </Link>
-            </div>
-            */}
 
         </main>
     )
