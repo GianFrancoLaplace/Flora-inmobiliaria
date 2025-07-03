43211cce2cbd149a1eb75b59bbce1242a2265b62
Agostina Bidegain <agostinabidegain@gmail.com>
2025-07-02 23:13:57 -0300

    fix empty space at the beggining of small properties grid

--- src/components/BigCards/BigCardsGrid.tsx
+++ src/components/BigCards/BigCardsGrid.tsx
@@ -1,10 +1,22 @@
 import styles from './BigCard.module.css'
 import {cactus} from "@/app/(views)/ui/fonts";
 import BigCard from "@/components/BigCards/BigCard";
+import Link from "next/link";
+import React from "react";
 
 export default function BigCardsGrid() {
     return(
         <main className={`${styles.grid} ${cactus.className}`}>
+            {/*
+            <div
+                className={styles.messageButtonProperties}>
+                <Link href={"https://wa.me/2494025527"} className={styles.linkProperties}>
+                    <button className={`${styles.messageBtn} ${cactus.className}`}>Enviar un mensaje
+                    </button>
+                </Link>
+            </div>
+            */}
+
             <BigCard
                 imageSrc={"/backgrounds/fichaBackground.jpg"}
                 price={300000}
@@ -75,6 +87,16 @@ export default function BigCardsGrid() {
                 dorms={3}
                 bathrooms={2}
             />
+
+            {/*
+            <div className={`${styles.mainCardsGridProperties} ${isHome ? styles.viewButton : styles.notViewButton}`}>
+                <Link href={"/Propiedades"} className={styles.linkProperties}>
+                    <button className={`${styles.allPropertiesBtn} ${cactus.className}`}>Ver todas las propiedades
+                    </button>
+                </Link>
+            </div>
+            */}
+
         </main>
     )
 }
