a79fcaf8d6a77795d3de58a470aafb39b94bca30
GianFrancoLaplace <laplacegian@gmail.com>
2025-07-07 10:23:02 -0300

    refactorización del código de la ficha de propiedades, no funciona correctamente crear ni editar, compila

--- src/app/(views)/Propiedades/Sheet/page.tsx
+++ src/app/(views)/Propiedades/Sheet/page.tsx
@@ -1,10 +1,24 @@
-import FichaTecnica from '@/components/TechnicalFile/TechnicalSheet'
+import TechnicalSheet from '@/components/TechnicalFile/TechnicalSheet'
+import {PropertyState, PropertyType} from "@/components/TechnicalFile/Property";
 
 export default function FichaPropiedad() {
     return (
         <main>
             <div>
-                <FichaTecnica />
+                <TechnicalSheet mode={'view'} property={
+                    {
+                        id: 0,
+                        address: "",
+                        city: "",
+                        description: "",
+                        price: 0,
+                        state: PropertyState.RENT,
+                        type: PropertyType.HOME,
+                        bathrooms: 0,
+                        bedrooms: 0,
+                        squareMeters: 0
+                    }
+                }  />
             </div>
         </main>
     )
