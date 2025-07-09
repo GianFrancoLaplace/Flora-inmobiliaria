57f8c16b59097e26e348260354c676df3c9636e9
milagros alvarez <milagrosalvarez2604@gmail.com>
2025-07-09 17:31:54 -0300

    agrego helper para mapear a objeto propiedad

---
+++ src/helpers/PropertyMapper.ts
@@ -0,0 +1,31 @@
+import { PropertyState, PropertyType } from "@/lib/definitions";
+
+export function mapOperationToState(operation: string): PropertyState {
+  switch (operation) {
+    case 'venta':
+      return PropertyState.SALE;
+    case 'alquiler':
+      return PropertyState.RENT;
+    default:
+      return PropertyState.SALE;
+  }
+}
+
+export function mapPropertyType(type: string): PropertyType {
+  switch (type) {
+    case 'casa':
+      return PropertyType.HOME;
+    case 'departamento':
+      return PropertyType.APARTMENT;
+    case 'campo':
+      return PropertyType.FIELD;
+    case 'duplex':
+      return PropertyType.DUPLEX;
+    case 'local_comercial':
+      return PropertyType.COMMERCIAL;
+    case 'terreno':
+      return PropertyType.LAND;
+    default:
+      return PropertyType.HOME; // valor por defecto o error si querés ser estricta
+  }
+}
