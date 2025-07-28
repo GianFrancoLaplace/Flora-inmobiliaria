d3b8913af06ff5a4416ec7bf419622fb0edacaba
milagros alvarez <milagrosalvarez2604@gmail.com>
2025-07-28 16:13:50 -0300

    arreglo pasaje a ingles en get de routets

--- src/app/api/properties/route.ts
+++ src/app/api/properties/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 import { PropertyService } from '@/services/propertyService';
 import { Property } from '@/types/Property';
-import { Characteristic} from "@/types/Characteristic";
+import { Characteristic } from "@/types/Characteristic";
 import { mapOperationToState, mapPropertyType } from '@/helpers/PropertyMapper';
 import { mapPrismaCharacteristicCategory } from '@/helpers/IconMapper';
 import image from 'next/image';
@@ -18,11 +18,11 @@ export async function GET(request: Request) {
     try {
         const { searchParams } = new URL(request.url);
 
-        const tipos = searchParams.get('tipo')?.split(',') ?? undefined;
-        const operaciones = searchParams.get('operacion')?.split(',') ?? undefined;
+        const types = searchParams.get('tipo')?.split(',') ?? undefined;
+        const operations = searchParams.get('operacion')?.split(',') ?? undefined;
         const maxValue = searchParams.get('maxValue');
 
-        const service = new PropertyService(tipos, operaciones);
+        const service = new PropertyService(types, operations);
         const where = service.buildWhereClause();
 
         if (maxValue && !isNaN(Number(maxValue))) {
@@ -35,42 +35,42 @@ export async function GET(request: Request) {
             }
         }
 
-        const propiedadesRaw = await prisma.property.findMany({
-    where: Object.keys(where).length > 0 ? where : undefined,
-    include: {
-        characteristics: true,
-        image: true,
-    },
-});
+        const propertiesRaw = await prisma.property.findMany({
+            where: Object.keys(where).length > 0 ? where : undefined,
+            include: {
+                characteristics: true,
+                image: true,
+            },
+        });
 
 
-        const propiedades: Property[] = propiedadesRaw.map((p) => ({
-    id: p.id_property,
-    address: p.address || '',
-    city: '',
-    state: mapOperationToState(p.category),
-    price: p.price || 0,
-    description: p.description || '',
-    type: mapPropertyType(p.type),
-    characteristics: p.characteristics.map((c): Characteristic => ({
-        id: c.id_characteristic,
-        characteristic: c.characteristic,
-        data_type: c.data_type === 'integer' ? 'integer' : 'text',
-        value_integer: c.value_integer ?? undefined,
-        value_text: c.value_text?.trim() || undefined,
-        category: mapPrismaCharacteristicCategory(c.category || null),
-    })),
-    ubication: p.ubication || '',
+        const properties: Property[] = propertiesRaw.map((p) => ({
+            id: p.id_property,
+            address: p.address || '',
+            city: '',
+            state: mapOperationToState(p.category),
+            price: p.price || 0,
+            description: p.description || '',
+            type: mapPropertyType(p.type),
+            characteristics: p.characteristics.map((c): Characteristic => ({
+                id: c.id_characteristic,
+                characteristic: c.characteristic,
+                data_type: c.data_type === 'integer' ? 'integer' : 'text',
+                value_integer: c.value_integer ?? undefined,
+                value_text: c.value_text?.trim() || undefined,
+                category: mapPrismaCharacteristicCategory(c.category || null),
+            })),
+            ubication: p.ubication || '',
 
-    images: (() => {
-    const portada = p.image[0];
-    return portada ? [{ id: portada.id_image, url: portada.url! }] : [];
-})(),
-}));
+            images: (() => {
+                const mainimage = p.image[0];
+                return mainimage ? [{ id: mainimage.id_image, url: mainimage.url! }] : [];
+            })(),
+        }));
 
 
 
-        return NextResponse.json(propiedades);
+        return NextResponse.json(properties);
     } catch (error) {
         console.error('Error en GET /api/properties:', error);
         return new NextResponse('Error al obtener propiedades', { status: 500 });
