8ff7e60530753a25afab716f45ec1ba16f0fc859
milagros alvarez <milagrosalvarez2604@gmail.com>
2025-08-08 21:00:34 -0300

    fuerzo al route de caracteristicas a no hacer prerender

--- src/app/api/characteristics/[id]/route.ts
+++ src/app/api/characteristics/[id]/route.ts
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { CharacteristicCategory, CharacteristicCreate, CharacteristicValidationInput, Characteristic } from "@/types/Characteristic";
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
