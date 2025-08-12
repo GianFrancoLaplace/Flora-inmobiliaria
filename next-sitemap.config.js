7e8504204ef9f1c81bdaff9bed1c6b3f0e665c76
milagros alvarez <milagrosalvarez2604@gmail.com>
2025-08-12 11:38:06 -0300

    agrego configuracion de sitemap para SEO

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: 'https://flora-cordeiro-inmobiliaria.vercel.app',
+  generateRobotsTxt: true,
+};
+
+export default config;
